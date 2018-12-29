7c15af862e74982f848020d6a0c1f2584788939c
Marcos Antonio Mateus <mateuslczo@gmail.com>
2018-12-29 19:05:10 -0200

    criacao da funcao handler e definicao da port http

--- go/src/websocket/main.go
+++ go/src/websocket/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
-	var test = "teste"
+	http.HandleFunc("/", handler)     // Declaramos o path e o handler
+	http.ListenAndServe(":3000", nil) // Indicamos a porta em que nosso WebServer irá responder
 
-	fmt.Println("HelloWorld: " + test)
+}
+
+// Tratamos nossa requisição: o writer irá permitir definir o que desejamos escrever de volta para
+// o client que eniou a requisição
+func handler(writer http.ResponseWriter, request *http.Request) {
+
+	fmt.Fprint(writer, "Hello")
 
 }
