570272ab5317261a7335a5ea40b508303175e95c
Marcos Antonio Mateus <mateuslczo@gmail.com>
2018-12-29 19:53:12 -0200

    finalizacao do app

--- go/src/websocket/main.go
+++ go/src/websocket/main.go
@@ -1,9 +1,20 @@
+// No menu DEMOS > Echo Test do site http://websocket.org é possível o funcionamento
 package main
 
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+// Uma das propriedades de o upgrader é checar a origem do trafego
+var upgrader = websocket.Upgrader{
+
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
 func main() {
 
@@ -16,6 +27,31 @@ func main() {
 // o client que eniou a requisição
 func handler(writer http.ResponseWriter, request *http.Request) {
 
-	fmt.Fprint(writer, "Hello")
+	socket, err := upgrader.Upgrade(writer, request, nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for {
+		// Le a mensagem recebida via WebSocket
+		msgType, msg, err := socket.ReadMessage()
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Loga no console do WebWriter
+		fmt.Println("Mensagem recebida: ", string(msg))
+
+		// Devolve a mensagem recebida de volta para o cliente
+		err = socket.WriteMessage(msgType, msg)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+	}
 
 }
